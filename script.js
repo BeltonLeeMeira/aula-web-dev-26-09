9852d0c96a8dc74230252a0e73bc2f287aaed116
Belton <blcdmmeira@gmail.com>
2024-09-26 10:49:41 -0300

    Criando exemplo e declaração de variaveis

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ console.log(exemplo2)
 
 const exemplo3 = "Cidade"
 console.log(exemplo3)
+
+//valor undefined
+let valor;
+console.log(valor)
+
+
+//valor nulo
+let valor1 = null;
+console.log(valor1)
+
+//valor booleano
+
+let valor2 = false
+console.log(valor2)
+
+//valor objeto
+let objeto = {};
+console.log(objeto)
+
+// array
+
+let array = [];
+console.log(array)
+
+let array1 = [];
+console.log(array1)
+
